cbe523af1bd10f8e5be56fe880b85e939c1fb154
LifyAIapp <lifyorg@gmail.com>
2025-06-27 20:36:18 +0300

    Update db_friends.py

--- database/db_friends.py
+++ database/db_friends.py
@@ -33,7 +33,7 @@ async def get_friends(user_id: str):
             """
             SELECT friend_id, role FROM friends
             WHERE user_id = $1
-            ORDER BY added_at
+            ORDER BY created_at
             """,
             user_id
         )
